6052c10a0831fc54e09de11e9be7e10a3dc6f79c
Kaleert <igprpro003gusin@gmail.com>
2026-09-05 11:05:18 +0300

    .

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -96,6 +96,7 @@ abstract class AppLocalizations {
   static const List<Locale> supportedLocales = <Locale>[
     Locale('en'),
     Locale('ru'),
+    Locale('uk'),
   ];
 
   /// No description provided for @loginTitle.
@@ -5366,7 +5367,7 @@ class _AppLocalizationsDelegate
 
   @override
   bool isSupported(Locale locale) =>
-      <String>['en', 'ru'].contains(locale.languageCode);
+      <String>['en', 'ru', 'uk'].contains(locale.languageCode);
 
   @override
   bool shouldReload(_AppLocalizationsDelegate old) => false;
@@ -5379,6 +5380,8 @@ AppLocalizations lookupAppLocalizations(Locale locale) {
       return AppLocalizationsEn();
     case 'ru':
       return AppLocalizationsRu();
+    case 'uk':
+      return AppLocalizationsUk();
   }
 
   throw FlutterError(
--- lib/l10n/terms_of_service.dart
+++ lib/l10n/terms_of_service.dart
@@ -2,10 +2,14 @@ import 'package:flutter/widgets.dart';
 
 import 'tos_en.dart';
 import 'tos_ru.dart';
+import 'tos_uk.dart';
 
 String termsOfServiceBody(Locale locale) {
   if (locale.languageCode == 'ru') {
     return kTermsOfServiceRu;
+  }
+  if (locale.languageCode == 'uk') {
+    return kTermsOfServiceUk;
   }
   return kTermsOfServiceEn;
 }
